cb10270b1cea801538a1fcb87566b881aa7a16a6
Jamal Yauhari Martinez <jamalyauharimcollege@gmail.com>
2020-01-14 23:12:45 -0500

    action sheet added

--- Source/View Controller/ProfileViewController/ProfileImageViewControllerExtension.swift
+++ Source/View Controller/ProfileViewController/ProfileImageViewControllerExtension.swift
@@ -10,7 +10,27 @@ import UIKit
 
 extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    internal func presentCameraPickerViewController() {
+    func showUIImagePickerControllerActionSheet() {
+        let imageFromLibraryAction = UIAlertAction(title: "Choose from Library", style: .default) { (action) in
+            self.presentCameraPickerViewController(source: .photoLibrary)
+        }
+        
+        let takePictureAction = UIAlertAction(title: "Take Photo", style: .default) { (action) in
+            self.presentCameraPickerViewController(source: .camera)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let actionSheet = UIAlertController(title: "Change Profile Photo", message: "", preferredStyle: .actionSheet)
+        actionSheet.addAction(imageFromLibraryAction)
+        actionSheet.addAction(takePictureAction)
+        actionSheet.addAction(cancelAction)
+        
+        
+        self.present(actionSheet, animated: true)
+    }
+    
+    internal func presentCameraPickerViewController(source: UIImagePickerController.SourceType) {
         let imagePickerViewController = UIImagePickerController()
         imagePickerViewController.delegate = self
         imagePickerViewController.allowsEditing = true

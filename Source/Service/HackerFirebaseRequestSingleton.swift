09d1e8fd1ae780cd7c2491d8e56e90d1f4f1317e
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2020-02-06 22:50:29 -0500

    add group data

--- Source/Service/HackerFirebaseRequestSingleton.swift
+++ Source/Service/HackerFirebaseRequestSingleton.swift
@@ -12,6 +12,56 @@ import Firebase
 public class HackerFirebaseRequestSingleton {
     
     // get points, groups, and name
+    static let databaseReference = Database.database().reference()
+    static func getCompleteHackerData(input: HackerModel, completion: @escaping (HackerModel) -> Void) {
+        databaseReference.child("administrative_fields").queryOrdered(byChild: "publicUuid").queryEqual(toValue: input.uuid.publicUUID).observeSingleEvent(of: .value) { (snapshot) in
+            DispatchQueue.main.async {
+                guard let options = snapshot.value as? [String: Any], let value = options.first?.value as? [String: Any] else {
+                    completion(input)
+                    return
+                }
+                
+                var output = input
+                if let foodGroup = value["foodGroup"] as? String {
+                    output.foodGroup = foodGroup
+                }
+                
+                if let pointsGroup = value["pointsGroup"] as? String {
+                    output.pointsGroup = pointsGroup
+                }
+                
+                if let pointsCount = value["pointsCount"] as? Int {
+                    output.points = pointsCount
+                }
+                
+                if let privateUUID = value["privateUuid"] as? String {
+                    output.privateUUID = privateUUID
+                }
+                
+                addHackerNameToData(input: output, completion: { (completeOutput) in
+                    completion(completeOutput)
+                })
+            }
+        }
+    }
+    
+    static func addHackerNameToData(input: HackerModel, completion: @escaping (HackerModel) -> Void) {
+        databaseReference.child("searchHacker").queryOrdered(byChild: "privateUuid").queryEqual(toValue: input.privateUUID).observeSingleEvent(of: .value) { (snapshot) in
+            DispatchQueue.main.async {
+                guard let options = snapshot.value as? [String: Any], let value = options.first?.value as? [String: Any] else {
+                    completion(input)
+                    return
+                }
+                
+                var output = input
+                if let name = value["name"] as? String {
+                    output.name = name
+                }
+                
+                completion(output)
+            }
+        }
+    }
     
     // get event endtime
     static let storage = Storage.storage()

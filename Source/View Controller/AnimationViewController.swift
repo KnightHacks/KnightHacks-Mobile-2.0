94ab95df2d5be6b7b5429eaa32c811a1af8809d1
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2020-02-10 23:08:06 -0500

    fix loading animation and live update initial color

--- Source/View Controller/AnimationViewController.swift
+++ Source/View Controller/AnimationViewController.swift
@@ -20,23 +20,31 @@ public class AnimationHolder: UIViewController {
     init(frame: CGRect) {
         super.init(nibName: nil, bundle: nil)
         self.animationView = AnimationView(frame: frame)
-        
         self.view = self.animationView
         self.view.frame = frame
         self.view.backgroundColor = UIColor(red: 23, green: 46, blue: 115, a: 1)
+        self.addSideCovers()
     }
     
     override public func viewDidAppear(_ animated: Bool) {
         let starAnimation = Animation.named("light-launch-animation")
-        animationView.animationSpeed = 1.3
+        animationView.animationSpeed = 1.4
         animationView.animation = starAnimation
-        animationView.contentMode = .scaleAspectFit
+        animationView.contentMode = .scaleAspectFill
         animationView.play { (_) in
             let mainBundle: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let mainViewController = mainBundle.instantiateViewController(withIdentifier: "MainNavigationViewController")
             mainViewController.modalTransitionStyle = .crossDissolve
             self.present(mainViewController, animated: true, completion: nil)
         }
+    }
+    
+    private func addSideCovers() {
+        let left = UIView(frame: self.view.frame)
+        left.backgroundColor = UIColor(red: 23, green: 46, blue: 115, a: 1)
+        
+        let right = UIView(frame: self.view.frame)
+        right.backgroundColor = UIColor(red: 23, green: 46, blue: 115, a: 1)
     }
     
     required init?(coder aDecoder: NSCoder) {
